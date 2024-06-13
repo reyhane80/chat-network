64db95afada1a05190b3a0171527d4db5e0df333
reyhane80 <163353832+reyhane80@users.noreply.github.com>
2024-06-13 04:47:31 -0700

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+public class TcpClientSample
+{
+    
+    private const string serverIp = "127.0.0.1";
+    private const int port = 5000;
+
+    public static void Main()
+    {
+        TcpClient server;
+        try
+        {
+            server = new TcpClient(serverIp, port);
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine("Unable to connect to server");
+            return;
+        }
+        Console.WriteLine("Connected to the Server...");
+        NetworkStream ns = server.GetStream();
+
+        SendFile("test.txt", ns);
+
+        ns.Close();
+        server.Close();
+    }
+
+    public static void SendFile(string fileName, NetworkStream ns)
+    {
+
+        byte[] fileData = File.ReadAllBytes(fileName);
+
+
+        byte[] fileDataLength = BitConverter.GetBytes(fileData.Length);
+        ns.Write(fileDataLength, 0, fileDataLength.Length);
+
+
+        ns.Write(fileData, 0, fileData.Length);
+        ns.Flush();
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static TcpListener tcpServer;
+        static Thread th;
+
+        public static void Main()
+        {
+            th = new Thread(new ThreadStart(StartListen));
+            th.Start();
+        }
+
+        public static void StartListen()
+        {
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            tcpServer = new TcpListener(localAddr, 5000);
+            tcpServer.Start();
+
+            while (true)
+            {
+                TcpClient tcpClient = tcpServer.AcceptTcpClient();
+                NetworkStream ns = tcpClient.GetStream();
+
+                ReceiveFile(ns);
+
+                ns.Close();
+                tcpClient.Close();
+            }
+        }
+
+        public static void ReceiveFile(NetworkStream ns)
+        {
+
+            byte[] fileDataLengthBytes = new byte[4];
+            ns.Read(fileDataLengthBytes, 0, 4);
+            int fileDataLength = BitConverter.ToInt32(fileDataLengthBytes, 0);
+
+            byte[] fileData = new byte[fileDataLength];
+            int bytesRead = 0;
+            int totalBytesRead = 0;
+
+            while (totalBytesRead < fileDataLength)
+            {
+                bytesRead = ns.Read(fileData, totalBytesRead, fileDataLength - totalBytesRead);
+                totalBytesRead += bytesRead;
+            }
+
+            File.WriteAllBytes("received_file", fileData);
+        }
+    }
+}
